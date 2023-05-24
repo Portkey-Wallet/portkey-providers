613807efe271a5b92715d3eed055638c1f8856d6
portkey-yellow <yellow.m@portkey.finance>
2023-05-24 11:16:02 +0800

    feat: 🎸 example

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { IProvider } from '@portkey/provider-types';
+import { IChain, IContract, IWeb3Provider } from '@portkey/provider-types';
 import detectProvider from '@portkey/detect-provider';
+import './index.css';
 function App() {
-  const [provider, setProvider] = useState<IProvider>();
+  const [provider, setProvider] = useState<IWeb3Provider>();
+  const [chain, setChain] = useState<IChain>();
+  const [tokenContract, setTokenContract] = useState<IContract>();
   return (
     <div>
       <button
@@ -15,6 +18,54 @@ function App() {
           }
         }}>
         init provider
+      </button>
+      <button
+        onClick={async () => {
+          const _chain = provider.getChain('AELF');
+          setChain(_chain);
+          setTokenContract(_chain.getContract('JRmBduh4nXWi1aXgdUsj5gJrzeZb2LxmrAbf7W99faZSvoAaE'));
+        }}>
+        getChain
+      </button>
+      <button
+        onClick={async () => {
+          try {
+            const height = await chain.getBlockHeight();
+            console.log(height, '====height');
+            alert(height);
+          } catch (error) {
+            console.log(error, '====error');
+          }
+        }}>
+        getBlockHeight
+      </button>
+      <button
+        onClick={async () => {
+          try {
+            const balance = await tokenContract.callViewMethod('GetBalance', {
+              symbol: 'ELF',
+              owner: 'LSWoBaeoXRp9QW75mCVJgNP4YurGi2oEJDYu3iAxtDH8R6UGy',
+            });
+            console.log(balance, '=====balance');
+          } catch (error) {
+            console.log(error, '====error');
+          }
+        }}>
+        GetBalance
+      </button>
+      <button
+        onClick={async () => {
+          try {
+            const balance = await tokenContract.callSendMethod('GetBalance', '', {
+              symbol: 'ELF',
+              owner: 'LSWoBaeoXRp9QW75mCVJgNP4YurGi2oEJDYu3iAxtDH8R6UGy',
+            });
+            console.log(balance, '=====balance');
+          } catch (error) {
+            console.log(error, '====error');
+          }
+        }}>
+        callSendMethod
       </button>
     </div>
   );
