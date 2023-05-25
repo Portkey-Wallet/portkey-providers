657a28364c591fa76d5528ec1e2e1a9542a4d24f
portkey-yellow <yellow.m@portkey.finance>
2023-05-25 13:49:49 +0800

    feat: 🎸 readable-stream

--- packages/types/src/stream.ts
+++ packages/types/src/stream.ts
@@ -1,4 +1,4 @@
-import type { Duplex } from 'stream';
+import type { Duplex } from 'readable-stream';
 export interface IDappInteractionStream extends Duplex {
   createSubStream(name: string): void;
   createMessageEvent(message: string): void;
